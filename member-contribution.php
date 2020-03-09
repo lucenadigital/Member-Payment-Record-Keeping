e8818f06d0d101d121eb9b6d6c3a21f87c60b7aa
Michael <61347143+lucenadigital@users.noreply.github.com>
2020-03-09 23:27:38 +0800

    Update member-contribution.php
    set autocomplete:off

--- member-contribution.php
+++ member-contribution.php
@@ -2,23 +2,59 @@
 /**
  * Plugin Name:       Member Contribution
  * Plugin URI:        http://www.michaelcastrillo.com
- * Description:       Store and record payment of individual member.
+ * Description:       Just Insert Data into Custom Form
  * Version:           1.0
  * Author:            Lucena Digital Space
  * Author URI:        http://www.michaelcastrillo.com
  */
+ if (isset($_POST['submit'])) {
+	 $submitpayment = $_POST['submit'];
+	 if (!empty($submitpayment)) {
+		
+		global $wpdb;
+		$table_name ='wp_mcmuserpayment';
+		$accountnumber = $_POST['accountnumber'];
+		$monthlydues = $_POST['monthlydues'];
+		$medicalpayment = $_POST['medicalpayment'];
+		$damayangfe = $_POST['damayangfe'];
+		$nextduedate = $_POST['nextduedate'];
+		$totalmonthlypayment = $_POST['totalmonthlypayment'];
  
+		$success = $wpdb->insert("wp_mcmuserpayment", array(
+			"accountnumber" => $accountnumber,
+			"monthlydues" => $monthlydues,
+			"medicalpayment" => $medicalpayment,
+			"damayangfe" => $damayangfe,
+			"totalmonthlypayment" => $totalmonthlypayment,
+			"nextduedate" => $nextduedate,
+			"dateofpayment" => current_time('mysql'),
+		));
+
+		$success1 = $wpdb->update('wp_participants_database', array('next_due_date' => $nextduedate), array('account_number' => $accountnumber));
+
+		if($success) {
+			echo '<div class="alert-payment-success">Payment Success!</div>';
+		} else {
+			echo '<div class="alert-payment-failed">Account number not found</div>';
+		}
+}
+else { 
+	echo "Empty";
+}
+ }
+
 function custom_payment_form() {
+
  ?>
 
 <div class="form payment-form">
  <form action ="<?php echo $_SERVER['REQUEST_URI']; ?>" method ="post"> 
- <div class="form-row col-4"><label for name=""> Account Number: </label><input type="text" name="accountnumber" id ="account-number" placeholder="Enter account number"></div>
- <div class="form-row col-4"><label for name="MmonthlDues"> Monthly Dues: </label><input type="text" name="monthlydues" id ="monthly-dues" class="input monthlydues"></div>
- <div class="form-row col-4"><label for name="MedicalPayment"> Medical Payment: </label><input type="text" name="medicalpayment" id = "medical-payment" class="input medicalpayment" ></div>
- <div class="form-row col-4"><label for name="DamayangFE"> Damayang FE: </label><input type="text" name="damayangfe" id = "damayang-fe" placeholder=""></div>
- <div class="form-row col-4"><label > Total Monthly Payment: </label><input type="text" name="totalmonthlypayment" id="total-monthly-payment" onblur="getTotal()"></div>
- <div class="form-row col-4"><label for name="nextduedate"> Next Due Date: </label><input type="date" name="nextduedate" id = "next-duedate" placeholder="Enter next due date"></div>
+ <div class="form-row col-4"><label for name=""> Account Number: </label><input type="text" name="accountnumber" id ="account-number" placeholder="Enter account number" autocomplete="off"></div>
+ <div class="form-row col-4"><label for name="MmonthlDues"> Monthly Dues: </label><input type="text" name="monthlydues" id ="monthly-dues" class="input monthlydues" autocomplete="off"></div>
+ <div class="form-row col-4"><label for name="MedicalPayment"> Medical Payment: </label><input type="text" name="medicalpayment" id = "medical-payment" class="input medicalpayment" autocomplete="off"></div>
+ <div class="form-row col-4"><label for name="DamayangFE"> Damayang FE: </label><input type="text" name="damayangfe" id = "damayang-fe" placeholder="" autocomplete="off"></div>
+ <div class="form-row col-4"><label > Total Monthly Payment: </label><input type="text" name="totalmonthlypayment" id="total-monthly-payment" onblur="getTotal()" autocomplete="off"></div>
+ <div class="form-row col-4"><label for name="nextduedate"> Due Date: </label><input type="text" name="nextduedate" id = "next-duedate" placeholder="Enter next due date" autocomplete="off"></div>
  </div>
  <input type ="text" name ="date-payment-encoded" id ="date-payment-encoded" hidden>
  <div class="form-row col-" style="clear:both;"><input type="submit" name="submit" value="Add payment" onclick="return callValidation();this.disabled=true; this.value='Sending…';"></div>
@@ -67,38 +103,7 @@ function callValidation(){
 }
 add_shortcode('display_form', 'custom_payment_form');
 
-if (!isset($_POST['submit'])) {
-	//echo "Not valid";
-}
-else {
 
- global $wpdb;
- $table_name ='wp_mcmuserpayment';
- $accountnumber = $_POST['accountnumber'];
- $monthlydues = $_POST['monthlydues'];
- $medicalpayment = $_POST['medicalpayment'];
- $damayangfe = $_POST['damayangfe'];
- $nextduedate = $_POST['nextduedate'];
- $totalmonthlypayment = $_POST['totalmonthlypayment'];
- 
- $success = $wpdb->insert("wp_mcmuserpayment", array(
-   "accountnumber" => $accountnumber,
-   "monthlydues" => $monthlydues,
-   "medicalpayment" => $medicalpayment,
-   "damayangfe" => $damayangfe,
-   "totalmonthlypayment" => $totalmonthlypayment,
-   "nextduedate" => $nextduedate,
-    "dateofpayment" => current_time('mysql'),
-));
-
- $success1 = $wpdb->update('wp_participants_database', array('next_due_date' => $nextduedate), array('account_number' => $accountnumber));
-
- if($success) {
- echo '<div class="alert-payment-success">Payment Success!</div>';
-      } else {
-   echo '<div class="alert-payment-failed">Account number not found</div>';
-   }
-}
 
 
 function display_custom_form() {
@@ -113,6 +118,7 @@ function display_custom_form() {
     <th>Damayang FE</td>
     <th>Total Monthly Payment</td>
     <th>Date of Payment</td>
+	    <th>Next Due Date</td>
     </tr>
             <?php
     $result = $wpdb->get_results ( 'SELECT * FROM wp_mcmuserpayment ORDER BY dateofpayment desc' );
@@ -124,7 +130,8 @@ function display_custom_form() {
     <?php echo "<td>".$return->medicalpayment."</td>"?>
     <?php echo "<td>".$return->damayangfe."</td>"?>
     <?php echo "<td>".$return->totalmonthlypayment."</td>"?>
-    <?php echo "<td>".$return->dateofpayment."</td></tr>"?>
+    <?php echo "<td>".$return->dateofpayment."</td>"?>
+	<?php echo "<td>".$return->nextduedate."</td></tr>"?>
 
 
         <?php } ?>
